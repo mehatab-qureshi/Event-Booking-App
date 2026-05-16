512dbdee4bcd6fe8f81608dd4ece450abbe98c4f
Mehatab Qureshi <mahetabqureshi5676@gmail.com>
2026-05-16 21:24:13 +0530

    fix

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://event-booking-app-qgiv.onrender.com',
+    baseURL: 'https://event-booking-app-qgiv.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
